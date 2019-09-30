5a94bd01e10e6e30e2edbda9f2ad5f83f86fe50d
alex-valdez <alexvaldez31@tamu.edu>
2019-09-30 10:28:39 -0500

    Changed package

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const ReactNative = require('react-native')
-const { Buffer } = require('buffer')
-const { NativeModules, DeviceEventEmitter } = ReactNative
-const BluetoothSerial = NativeModules.BluetoothSerial
+const ReactNative = require("react-native");
+const { Buffer } = require("buffer");
+const { NativeModules, DeviceEventEmitter } = ReactNative;
+const BluetoothSerial1 = NativeModules.BluetoothSerial;
 
 /**
- * Listen for available events
- * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
- * @param  {Function} handler Event handler
+ * Listen for available events
+ * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
+ * @param  {Function} handler Event handler
  */
-BluetoothSerial.on = (eventName, handler) => {
-  DeviceEventEmitter.addListener(eventName, handler)
-}
+BluetoothSerial1.on = (eventName, handler) => {
+  DeviceEventEmitter.addListener(eventName, handler);
+};
 
 /**
- * Stop listening for event
- * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
- * @param  {Function} handler Event handler
+ * Stop listening for event
+ * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
+ * @param  {Function} handler Event handler
  */
-BluetoothSerial.removeListener = (eventName, handler) => {
-  DeviceEventEmitter.removeListener(eventName, handler)
-}
+BluetoothSerial1.removeListener = (eventName, handler) => {
+  DeviceEventEmitter.removeListener(eventName, handler);
+};
 
 /**
- * Write data to device, you can pass string or buffer,
- * We must convert to base64 in RN there is no way to pass buffer directly
- * @param  {Buffer|String} data
- * @return {Promise<Boolean>}
+ * Write data to device, you can pass string or buffer,
+ * We must convert to base64 in RN there is no way to pass buffer directly
+ * @param  {Buffer|String} data
+ * @return {Promise<Boolean>}
  */
-BluetoothSerial.write = (data) => {
-  if (typeof data === 'string') {
-    data = new Buffer(data)
+BluetoothSerial1.write = data => {
+  if (typeof data === "string") {
+    data = new Buffer(data);
   }
-  return BluetoothSerial.writeToDevice(data.toString('base64'))
-}
+  return BluetoothSerial1.writeToDevice(data.toString("base64"));
+};
 
-module.exports = BluetoothSerial
+module.exports = BluetoothSerial1;
